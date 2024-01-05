6bcb79481ceb5381be5c3596c398c9f2b1678486
Madhav Malhotra <madhav.malhotra003@gmail.com>
2024-01-05 09:38:22 -0500

    Removing experimental script for new bio data format

--- tmp.py
+++
@@ -1,9 +0,0 @@
-import json
-
-bios = json.load(open('./data/bios.json', 'r'))
-updated = []
-for key in bios.keys():
-    updated.append({'name': key, 'bio': bios[key]})
-
-with open('./data/bios.json', 'w') as f:
-    json.dump(updated, f)
